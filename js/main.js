916d1cccef615ea67dd83fcc5da12ad1d6fc3862
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2020-06-24 11:43:28 +0100

    added instagram feed

--- js/main.js
+++ js/main.js
@@ -91,6 +91,10 @@ $(document).ready(function () {
             $grid.isotope('layout');
         });
     });
+    (function (d, s, id) {
+        var js; if (d.getElementById(id)) { return; } js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/embedscript/in.js";
+        d.getElementsByTagName("head")[0].appendChild(js);
+    }(document, "script", "EmbedSocialInstagramScript"));
 });
 
 
