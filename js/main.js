76b7d8be886b9f6e52e68e07cebb715878c1c28c
Pirasanthan Jesugeevegan <58918065+pirasanthan-jesugeevegan@users.noreply.github.com>
2021-05-20 18:11:12 +0100

    Update (#7)

--- js/main.js
+++ js/main.js
@@ -1,97 +1,84 @@
 $(document).ready(function () {
-    //FORM SELECT
-    $('select').formSelect(); // https://materializecss.com/select.html
-    // MODAL
-    $('.modal').modal();
-    // TABS
-    $('ul.tabs').tabs();
-    // SCROLLSPY
-    $('.scrollspy').scrollSpy();
+  //FORM SELECT
+  $('select').formSelect(); // https://materializecss.com/select.html
+  // MODAL
+  $('.modal').modal();
+  // TABS
+  $('ul.tabs').tabs();
+  // SCROLLSPY
+  $('.scrollspy').scrollSpy();
 
-    $('.fixed-action-btn').floatingActionButton();
+  $('.fixed-action-btn').floatingActionButton();
 
-    $('a[href*="#"]')
-        // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function () {
-            // On-page links
-            if (
-                location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-                location.hostname == this.hostname
-            ) {
-                // Figure out element to scroll to
-                var target = $(this.hash);
-                target = target.length
-                    ? target
-                    : $("[name=" + this.hash.slice(1) + "]");
-                // Does a scroll target exist?
-                if (target.length) {
-                    var headerHeight = 0;
-                    if ($(window).width() < 767) {
-                        headerHeight = 80;
-                    }
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $("html, body").animate(
-                        {
-                            scrollTop: target.offset().top - headerHeight
-                        },
-                        1000
-                    );
-                }
-            }
-        });
-
-    $(document).click(function (e) {
-        $(".site-wrapper").removeClass("show-menu");
-    });
-
-    $("a.btn.responsive-menu").click(function (e) {
-        e.stopPropagation();
-        $(".site-wrapper").toggleClass("show-menu");
-    });
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 40) {
-            $("body").addClass("scrolled");
-        } else {
-            $("body").removeClass("scrolled");
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function () {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          var headerHeight = 0;
+          if ($(window).width() < 767) {
+            headerHeight = 80;
+          }
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate(
+            {
+              scrollTop: target.offset().top - headerHeight,
+            },
+            1000
+          );
         }
+      }
     });
 
+  $(document).click(function (e) {
+    $('.site-wrapper').removeClass('show-menu');
+  });
 
+  $('a.btn.responsive-menu').click(function (e) {
+    e.stopPropagation();
+    $('.site-wrapper').toggleClass('show-menu');
+  });
 
-    $(window).resize(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+      $('body').addClass('scrolled');
+    } else {
+      $('body').removeClass('scrolled');
+    }
+  });
 
-        if ($(this).width() > 1023) {
-
-            $('.navigation-header').hide();
-
-        } else {
-
-            $('.navigation-header').show();
-
-        }
-
-    });
-
-
+  $(window).resize(function () {
+    if ($(this).width() > 1023) {
+      $('.navigation-header').hide();
+    } else {
+      $('.navigation-header').show();
+    }
+  });
 });
-
 
 // MixItUp 3
 $(function () {
-    $('#Container').mixItUp({
-        load: {
-            // filter: 'all'
-            filter: 'all'
-        },
-        controls: {
-            toggleFilterButtons: false
-        }
-    });
+  $('#Container').mixItUp({
+    load: {
+      // filter: 'all'
+      filter: 'all',
+    },
+    controls: {
+      toggleFilterButtons: false,
+    },
+  });
 });
-
-
